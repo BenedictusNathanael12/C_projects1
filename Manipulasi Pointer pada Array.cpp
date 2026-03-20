d676c8e300077ed54f19f3c2e6f618774bc6c042
BenedictusNathanael12 <bennathanael2012@gmail.com>
2026-03-21 01:57:50 +0700

    Membalikkan urutan elemen di dalam array secara langsung menggunakan manipulasi pointer memori.

---
+++ Manipulasi Pointer pada Array.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void reverseArray(int *arr, int size) {
+    int *start = arr;
+    int *end = arr + size - 1;
+    int temp;
+    
+    while (start < end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int numbers[] = {10, 20, 30, 40, 50};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    
+    reverseArray(numbers, size);
+    
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
