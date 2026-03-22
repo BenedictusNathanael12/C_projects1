e1e3e7779508ed06acc90aa0e047064b5389565a
BenedictusNathanael12 <bennathanael2012@gmail.com>
2026-03-22 22:17:13 +0700

    Menggunakan pengurangan (-) tinggi awal dengan target untuk mencari hasil akhir

---
+++ Pengurangan Tinggi.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int tinggi_awal = 15;
+    int target_turun = 4;
+    int tinggi_akhir = tinggi_awal - target_turun;
+    printf("%d\n", tinggi_akhir);
+    return 0;
+}
