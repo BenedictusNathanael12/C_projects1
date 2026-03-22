d9519af87ee807b44d63e6ed6ff1fa30449909f8
BenedictusNathanael12 <bennathanael2012@gmail.com>
2026-03-22 22:17:38 +0700

    Menjumlahkan (+) dua angka lalu membaginya (/) dua untuk mencari nilai tengah

---
+++ Nilai Rata-rata.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    float nilai_ujian1 = 85.5;
+    float nilai_ujian2 = 90.0;
+    float rata_rata = (nilai_ujian1 + nilai_ujian2) / 2.0;
+    printf("%.2f\n", rata_rata);
+    return 0;
+}
