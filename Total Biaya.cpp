b55e1f2a4e59c30dc4e6ea04ae316faf6fbc07ac
BenedictusNathanael12 <bennathanael2012@gmail.com>
2026-03-22 22:16:48 +0700

    Menggunakan perkalian (*) antara harga dan jumlah untuk mencari total biaya

---
+++ Total Biaya.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int harga_tiket = 250000;
+    int jumlah_orang = 4;
+    int total_biaya = harga_tiket * jumlah_orang;
+    printf("%d\n", total_biaya);
+    return 0;
+}
